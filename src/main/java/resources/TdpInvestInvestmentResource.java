d299b23fe4871699b8c6892d90cd5368ca035a84
Konrad Mrozewski <k.mrozewski@kainos.com>
2016-07-05 17:34:55 +0200

    add controller Investment

---
+++ src/main/java/resources/TdpInvestInvestmentResource.java
@@ -0,0 +1,14 @@
+package resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/investment/{investmentId}")
+@Produces(MediaType.APPLICATION_JSON)
+public class TdpInvestInvestmentResource {
+    @GET
+    public String fetch(@PathParam("investmentId") String investmentId) { return investmentId; }
+}
