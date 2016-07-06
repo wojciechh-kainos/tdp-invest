19ec5860b48503c9dfe79e418539a612d009ac76
wojciechh-kainos <w.hnatyszyn@kainos.com>
2016-07-06 17:34:06 +0200

    Revert unnecessary changes before merging

--- src/main/resources/assets/js/application/require-config.js
+++ src/main/resources/assets/js/application/require-config.js
@@ -3,7 +3,7 @@
 require.config({
     baseUrl: 'js/',
     paths: {
-        'angular': 'lib/angular/angular',
+        'angular': 'lib/angular/angular.min',
         'uiRouter': 'lib/angular-ui-router/release/angular-ui-router.min',
     },
     shim: {
