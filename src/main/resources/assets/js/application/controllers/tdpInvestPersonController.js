84b3838695788148ac95a6a602f1cae7285716e6
Malgorzata Rogowska <malgorzatar@gauth.local>
2016-07-07 08:54:01 +0200

    add highcharts library

--- src/main/resources/assets/js/application/controllers/tdpInvestPersonController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestPersonController.js
@@ -1,6 +1,8 @@
 define(['angular', 'application/tdpInvestModule'], function(angular, tdpInvestModule) {
     tdpInvestModule.controller("tdpInvestPersonController", function($scope, $stateParams) {
         $scope.personId = $stateParams.personId;
+        $scope.personName = '';
+        $scope.personId = '';
         $scope.getPersonId = function() {
             return $scope.personId;
         };
--- src/main/resources/assets/js/application/require-config.js
+++ src/main/resources/assets/js/application/require-config.js
@@ -5,12 +5,18 @@ require.config({
     paths: {
         'angular': 'lib/angular/angular.min',
         'uiRouter': 'lib/angular-ui-router/release/angular-ui-router.min',
+        'highcharts': 'lib/highcharts/highcharts',
+        'highcharts-ng': 'lib/highcharts-ng/dist/highcharts-ng.min'
     },
     shim: {
         'angular': {
             exports: 'angular'
         },
         'uiRouter' : ['angular'],
+        'highcharts': {
+            exports: 'highcharts'
+        },
+        'highcharts-ng' : ['angular', 'highcharts']
     },
     deps: ['application/bootstrap']
 });
