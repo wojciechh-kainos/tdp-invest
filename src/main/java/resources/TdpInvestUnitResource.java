751eb4840dacfd28bc5c0e1594ebf768d6c463ba
Malgorzata Rogowska <malgorzatar@gauth.local>
2016-07-08 17:41:34 +0200

    change response type to list

--- src/main/java/resources/TdpInvestUnitResource.java
+++ src/main/java/resources/TdpInvestUnitResource.java
@@ -2,26 +2,40 @@ package resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Path("/unit")
 @Produces(MediaType.APPLICATION_JSON)
 public class TdpInvestUnitResource {
+    private class Unit {
+        public String date;
+        public Double value;
+
+        public Unit(String a, Double b) {
+            this.date = a;
+            this.value = b;
+        }
+
+        
+    }
 
     @GET
-    public Map<String, Double> fetch() {
-        Map<String, Double> data = new HashMap<>();
-        data.put("05/01/1998", 200.0);
-        data.put("06/01/1998", 202.0);
-        data.put("07/01/1998", 204.0);
-        data.put("08/01/1998", 206.0);
-        data.put("09/01/1998", 208.0);
-        data.put("15/01/1998", 202.0);
-        data.put("25/01/1998", 200.0);
-        data.put("27/01/1998", 202.0);
+    public List<Unit> fetch() {
+        List<Unit> data = new ArrayList<>();
+        data.add(new Unit("2016-10-21", 200.0));
+        data.add(new Unit("2016-10-22", 202.0));
+        data.add(new Unit("2016-10-23", 204.0));
+        data.add(new Unit("2016-10-24", 206.0));
+        data.add(new Unit("2016-10-25", 208.0));
+        data.add(new Unit("2016-10-26", 202.0));
+        data.add(new Unit("2016-10-27", 200.0));
+        data.add(new Unit("2016-10-29", 202.0));
         return data;
     }
 }
