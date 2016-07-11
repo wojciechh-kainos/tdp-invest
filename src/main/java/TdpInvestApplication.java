41e52d3be809afd05c221d2281947003c471cdcd
Daniel Rauf <danielr@kainos.com>
2016-07-11 16:42:59 +0200

    Add authentication

--- src/main/java/TdpInvestApplication.java
+++ src/main/java/TdpInvestApplication.java
@@ -1,8 +1,13 @@
+import auth.TdpInvestAuthenticator;
 import com.github.dirkraft.dropwizard.fileassets.FileAssetsBundle;
 import configuration.TdpInvestApplicationConfiguration;
 import io.dropwizard.Application;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import model.User;
+import resources.TdpInvestAuthResource;
 import resources.TdpInvestPersonResource;
 
 
@@ -15,6 +20,12 @@ public class TdpInvestApplication extends Application<TdpInvestApplicationConfig
 
     @Override
     public void run(TdpInvestApplicationConfiguration configuration, Environment environment) {
+        environment.jersey().register(new AuthDynamicFeature(
+                new BasicCredentialAuthFilter.Builder<User>()
+                        .setAuthenticator(new TdpInvestAuthenticator())
+                        .buildAuthFilter()));
+
+        environment.jersey().register(new TdpInvestAuthResource());
         environment.jersey().register(new TdpInvestPersonResource());
     }
 
--- src/main/java/auth/TdpInvestAuthenticator.java
+++ src/main/java/auth/TdpInvestAuthenticator.java
@@ -1,17 +1,18 @@
 package auth;
 
 
+import com.google.common.base.Optional;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicCredentials;
 import model.User;
 
-import javax.security.sasl.AuthenticationException;
-import java.util.Optional;
 
 public class TdpInvestAuthenticator implements Authenticator<BasicCredentials, User> {
-        @Override
-        public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
-            if ("secret".equals(credentials.getPassword())) {
-                return Optional.of(new User(credentials.getUsername()));
-            }
-            return Optional.absent();
+    @Override
+    public Optional<User> authenticate(BasicCredentials credentials) {
+        if ("secret".equals(credentials.getPassword())) {
+            return Optional.of(new User(credentials.getUsername()));
         }
+        return Optional.absent();
     }
+}
