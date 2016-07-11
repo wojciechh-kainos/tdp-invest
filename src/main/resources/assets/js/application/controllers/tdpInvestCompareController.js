35d8b6e58bb0712156f307fb3fc72671ada1a8dc
Mateusz Czarnowski <m.m.czarnowski@gmail.com>
2016-07-11 13:41:29 +0200

    Add highcharts-ng

--- src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
@@ -37,5 +37,53 @@ define(['angular', 'application/tdpInvestModule'], function(angular, tdpInvestMo
         collection = generate(20);
         $scope.collection = collection;
 
+        $scope.chartConfig = {
+
+          options: {
+              //This is the Main Highcharts chart config. Any Highchart options are valid here.
+              //will be overriden by values specified below.
+              chart: {
+                  type: 'bar'
+              },
+              tooltip: {
+                  style: {
+                      padding: 10,
+                      fontWeight: 'bold'
+                  }
+              }
+          },
+          //The below properties are watched separately for changes.
+
+          //Series object (optional) - a list of series using normal Highcharts series options.
+          series: [{
+             data: [10, 15, 12, 8, 7]
+          }],
+          //Title configuration (optional)
+          title: {
+             text: 'Hello'
+          },
+          //Boolean to control showing loading status on chart (optional)
+          //Could be a string if you want to show specific loading text.
+          loading: false,
+          //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+          //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
+          xAxis: {
+          currentMin: 0,
+          currentMax: 20,
+          title: {text: 'values'}
+          },
+          //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
+          useHighStocks: false,
+          //size (optional) if left out the chart will default to size of the div or something sensible.
+          size: {
+           width: 400,
+           height: 300
+          },
+          //function (optional)
+          func: function (chart) {
+           //setup some logic for the chart
+          }
+        };
+
     });
 });
--- src/main/resources/assets/js/application/require-config.js
+++ src/main/resources/assets/js/application/require-config.js
@@ -5,12 +5,15 @@ require.config({
     paths: {
         'angular': 'lib/angular/angular.min',
         'uiRouter': 'lib/angular-ui-router/release/angular-ui-router.min',
+        'highcharts-ng': 'lib/highcharts-ng/dist/highcharts-ng.min',
+        'highcharts': 'http://code.highcharts.com/highcharts.src'
     },
     shim: {
         'angular': {
             exports: 'angular'
         },
         'uiRouter' : ['angular'],
+        'highcharts-ng': ['angular', 'highcharts']
     },
     deps: ['application/bootstrap']
 });
