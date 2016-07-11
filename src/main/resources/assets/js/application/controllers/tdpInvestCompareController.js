629350cecdba66a6375b51bbcc258f5a4c023d3b
Jakub Raniszewski <j.raniszewski@kainos.com>
2016-07-11 14:33:28 +0200

    configure stock chart

--- src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
@@ -37,13 +37,23 @@ define(['angular', 'application/tdpInvestModule'], function(angular, tdpInvestMo
         collection = generate(20);
         $scope.collection = collection;
 
+        //for chart
+        var prices = [];
+        var dates = [];
+        for(i=0; i<collection.length; i++){
+            prices.push(parseFloat(collection[i].price));
+            dates.push(collection[i].date);
+        }
+
+        console.log(prices);
+
         $scope.chartConfig = {
 
           options: {
               //This is the Main Highcharts chart config. Any Highchart options are valid here.
               //will be overriden by values specified below.
               chart: {
-                  type: 'bar'
+                  type: 'line'
               },
               tooltip: {
                   style: {
@@ -56,11 +66,12 @@ define(['angular', 'application/tdpInvestModule'], function(angular, tdpInvestMo
 
           //Series object (optional) - a list of series using normal Highcharts series options.
           series: [{
-             data: [10, 15, 12, 8, 7]
+                showInLegend : false,
+                data : prices
           }],
           //Title configuration (optional)
           title: {
-             text: 'Hello'
+             text: 'Stock prices'
           },
           //Boolean to control showing loading status on chart (optional)
           //Could be a string if you want to show specific loading text.
@@ -68,16 +79,19 @@ define(['angular', 'application/tdpInvestModule'], function(angular, tdpInvestMo
           //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
           //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
           xAxis: {
-          currentMin: 0,
-          currentMax: 20,
-          title: {text: 'values'}
+          title: {text: 'Date'},
+          categories : dates
+          },
+
+          yAxis: {
+              title: {text: 'Price'},
           },
           //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
           useHighStocks: false,
           //size (optional) if left out the chart will default to size of the div or something sensible.
           size: {
-           width: 400,
-           height: 300
+           width: 1200,
+           height: 400
           },
           //function (optional)
           func: function (chart) {
