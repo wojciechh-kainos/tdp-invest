3b48d3c3e8806f038e4137f78af86c20493690e6
Daniel Rauf <danielr@kainos.com>
2016-07-12 10:02:43 +0200

    Delete files from build folder added in prev commit

--- build/resources/main/assets/js/application/controllers/tdpInvestLoginController.js
+++
@@ -1,7 +0,0 @@
-define(['angular', 'application/tdpInvestModule', 'application/services/tdpPersonService'], function(angular, tdpInvestModule) {
-    tdpInvestModule.controller("tdpInvestLoginController", function($scope, $stateParams, tdpPersonService) {
-
-
-
-    });
-});
