7b0946b7932599a6846a7fb83849df0319d246a4
Mateusz Czarnowski <m.m.czarnowski@gmail.com>
2016-07-12 10:15:53 +0200

    Rename StockPrice.java

--- src/main/java/resources/TdpInvestCompareResource.java
+++ src/main/java/resources/TdpInvestCompareResource.java
@@ -2,7 +2,6 @@ package resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.text.DateFormat;
