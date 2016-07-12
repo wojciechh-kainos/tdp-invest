c76b50655e54e96de125def6d9fdf2ae9fd17387
Sara Cyganska <sarac@giant.local>
2016-07-12 13:34:02 +0200

    Add date filter on main page

--- src/main/resources/assets/js/application/controllers/tdpInvestMainController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestMainController.js
@@ -1,6 +1,42 @@
 define(['angular', 'application/tdpInvestModule', 'application/services/tdpDataService'], function(angular, tdpInvestModule) {
     tdpInvestModule.controller("tdpInvestMainController", function($scope, $stateParams, tdpDataService) {
-        var createChart = function(dateList, valuesList){
+        var investData = [];
+        tdpDataService.getInvestData().then(function(response){
+            investData = response.data;
+            var dateList = []
+            var valuesList = []
+            $scope.tableData = investData;
+
+            for (i = 0; i < response.data.length; i++) {
+                dateList.push(response.data[i].date.month);
+                valuesList.push(response.data[i].value)
+            }
+
+            createChart(dateList, valuesList);
+        });
+
+        $scope.submitRange = function(){
+            if(typeof $scope.start !== undefined && typeof $scope.end !== undefined){
+                var dateList = []
+                var valuesList = []
+                $scope.tableData = [];
+
+                var start = new Date($scope.start);
+                var end = new Date ($scope.end);
+                for (i = 0; i < investData.length; i++) {
+                    var date = new Date(investData[i].date.year + "-" + investData[i].date.monthValue + "-" + investData[i].date.dayOfMonth);
+                    if( date > start && date < end){
+                        $scope.tableData.push(investData[i]);
+                        dateList.push(investData[i].date.month);
+                        valuesList.push(investData[i].value)
+                    }
+                }
+
+                createChart(dateList, valuesList);
+            }
+        }
+
+        function createChart(dateList, valuesList){
             Highcharts.chart('container', {
                 xAxis: {
                     title: {
@@ -19,38 +55,6 @@ define(['angular', 'application/tdpInvestModule', 'application/services/tdpDataS
                     }]
                 });
         };
-
-        tdpDataService.getInvestData().then(function(response){
-            $scope.investData = response.data;
-
-            dateList = []
-            valuesList = []
-            for (i = 0; i < response.data.length; i++) {
-                dateList.push(response.data[i].date.month);
-                valuesList.push(response.data[i].value)
-            }
-
-            createChart(dateList, valuesList);
-        });
-
-        $scope.submitRange = function(){
-            if(typeof $scope.start !== undefined && typeof $scope.end !== undefined){
-
-                dateList = []
-                valuesList = []
-                var start = new Date($scope.start);
-                var end = new Date ($scope.end);
-                for (i = 0; i < $scope.investData.length; i++) {
-                    var date = new Date($scope.investData[i].date.year + "-" + $scope.investData[i].date.monthValue + "-" + $scope.investData[i].date.dayOfMonth);
-                    if( date > start && date < end){
-                        dateList.push($scope.investData[i].date.month);
-                        valuesList.push($scope.investData[i].value)
-                    }
-                }
-
-                createChart(dateList, valuesList);
-            }
-        }
 
     });
 });
