9e06f0e9b2c156dcf4ad9a813e1f0c6e81a05560
Konrad Mrozewski <k.mrozewski@kainos.com>
2016-07-12 10:06:54 +0200

    investment refactoring

--- src/main/java/helpers/TimeSeries.java
+++ src/main/java/helpers/TimeSeries.java
@@ -1,27 +1,37 @@
 package helpers;
 
 import model.Fund;
-
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-
-public abstract class TimeSeries {
-    public List<Fund> timeSeries;
-
-    public List<Fund> filterByDate(List<Fund> ts, Date startDate, Date endDate){
-        return ts
-                .stream()
-                .filter(row -> row.getDate().after(startDate) && row.getDate().before(endDate))
-                .collect(Collectors.toList());
+public class TimeSeries {
+    public List<Fund> createTimeSeries(Date startDate, Date endDate, double amount, double annualRate){
+        return Stream
+                .iterate(startDate, date -> addDays(date, 1))
+                .limit(datesDiffInDays(startDate, endDate) + 1)
+                .skip(1)
+                .map(row -> new Fund(row, futureValue(datesDiffInDays(startDate, row), amount, annualRate)))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public double profitInterval(double valStart, double valEnd, double amount){
-        throw new IllegalArgumentException();
+    private Date addDays(Date date, int days) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, days);
+
+        return cal.getTime();
     }
 
-    public double profitInterval(Date start, Date end, double amount, double annualRate){
-        throw new IllegalArgumentException();
+    private double futureValue(long days, double amount, double annualRate){
+        return amount * Math.pow(1 + annualRate / 365.25, days);
+    }
+
+    private long datesDiffInDays(Date startDate, Date endDate){
+        return TimeUnit.DAYS.convert(endDate.getTime() - startDate.getTime(), TimeUnit.MILLISECONDS);
     }
 }
