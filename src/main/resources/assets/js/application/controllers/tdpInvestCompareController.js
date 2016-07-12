23ee1731808e5a892a482e120880549f1662b650
Marcin Trendel <m.trendel@kainos.com>
2016-07-12 11:41:35 +0200

    Add form for compare page

--- src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
@@ -1,6 +1,58 @@
-define(['angular', 'application/tdpInvestModule', 'application/services/tdpInvestStockDataService'], function(angular, tdpInvestModule) {
+define(['angular', 'application/tdpInvestModule', 'application/services/tdpInvestStockDataService', 'ui-bootstrap'], function(angular, tdpInvestModule) {
     tdpInvestModule.controller("tdpInvestCompareController", ['$scope', '$stateParams', 'stockData', function($scope, $stateParams, stockData) {
         $scope.data = stockData.getData();
+
+        $scope.dateOptions = {
+            dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(1970, 1, 1),
+            startingDay: 1
+        };
+
+        // Disable weekend selection
+        function disabled(data) {
+            var date = data.date,
+            mode = data.mode;
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        }
+
+        $scope.open1 = function() {
+            $scope.popup1.opened = true;
+        };
+
+        $scope.open2 = function() {
+            $scope.popup2.opened = true;
+        };
+
+        $scope.format = 'dd/MM/yyyy';
+        $scope.altInputFormats = ['M!/d!/yyyy'];
+
+        $scope.popup1 = {
+            opened: false
+        };
+
+        $scope.popup2 = {
+            opened: false
+        };
+
+        function getDayClass(data) {
+            var date = data.date,
+            mode = data.mode;
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+                for (var i = 0; i < $scope.events.length; i++) {
+                    var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                    if (dayToCheck === currentDay) {
+                        return $scope.events[i].status;
+                    }
+                }
+            }
+
+            return '';
+        }
         
     }]);
 
