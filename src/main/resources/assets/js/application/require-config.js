643691c6a52024bc79c7f2b87dff4d6da8cbdb38
Mateusz Czarnowski <m.czarnowski@kainos.com>
2016-07-13 09:13:44 +0200

    Actually fix pagination Fix typo in calendar

--- src/main/resources/assets/js/application/require-config.js
+++ src/main/resources/assets/js/application/require-config.js
@@ -7,7 +7,7 @@ require.config({
         'uiRouter': 'lib/angular-ui-router/release/angular-ui-router.min',
         'highcharts-ng': 'lib/highcharts-ng/dist/highcharts-ng.min',
         'highcharts': 'http://code.highcharts.com/highcharts.src',
-        'angular-bootstrap': 'http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.12.1.min',
+//        'angular-bootstrap': 'http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.12.1.min',
         'ui-bootstrap' : 'lib/angular-bootstrap/ui-bootstrap-tpls',
         'angularMocks': 'lib/angular-mocks/angular-mocks',
         'eonasdan-bootstrap-datetimepicker' : 'lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker'
@@ -19,7 +19,7 @@ require.config({
         },
         'uiRouter' : ['angular'],
         'highcharts-ng': ['angular', 'highcharts'],
-        'angular-bootstrap': ['angular'],
+//        'angular-bootstrap': ['angular'],
         'ui-bootstrap' : ['angular']
     },
     deps: ['application/bootstrap']
