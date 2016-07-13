0f8a7797b88a1f97605e6b8121fd69aa59f9ebd5
Mateusz Czarnowski <m.czarnowski@kainos.com>
2016-07-13 13:42:57 +0200

    Refactor TDPInvestCompareController & HTML partial Adjust settings for PSQL

--- src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestCompareController.js
@@ -1,33 +1,17 @@
 define(['angular', 'application/tdpInvestModule', 'application/services/tdpCompareService'], function(angular, tdpInvestModule) {
-tdpInvestModule.controller("tdpInvestCompareController", function($scope, $stateParams, tdpCompareService) {
+    tdpInvestModule.controller("tdpInvestCompareController", function($scope, $stateParams, tdpCompareService) {
 
-    tdpCompareService.getData()
-        .then(function(response) {
-            //data came from java ;)
-
-            $scope.message = response.data;
-
-
-            //data
-            var collection = $scope.message;
-
-            //sort on price first
-            //sort - which column should be sorted
-            $scope.sort = 'price';
-            //reverse - if sort should be reverse or not
+        var initializeTable = function(response) {
+            $scope.sortType = 'price';
             $scope.reverse = false;
-            //we can sort only one column in one time
-
-            $scope.sortTable = function (sort){
-                if($scope.sort === sort){
-                    //if the same as previously sort should me reverse
+            $scope.sortTable = function(sortType) {
+                if ($scope.sortType === sortType) {
                     $scope.reverse = !$scope.reverse;
                 }
-                $scope.sort = sort;
+                $scope.sortType = sortType;
             }
-            $scope.collection = collection;
 
-            $scope.filteredCollection = [];
+            $scope.filteredStockData = [];
             $scope.currentPage = 1;
             $scope.numPerPage = 10;
             $scope.maxSize = 5;
@@ -36,72 +20,61 @@ tdpInvestModule.controller("tdpInvestCompareController", function($scope, $state
                 var begin = (($scope.currentPage - 1) * $scope.numPerPage),
                     end = begin + $scope.numPerPage;
 
-                $scope.filteredCollection = $scope.collection.slice(begin, end);
+                $scope.filteredStockData = $scope.stockData.slice(begin, end);
             });
 
-            //create data arrays for chart
-            var prices = [];
-            var dates = [];
-            for(i=0; i<$scope.collection.length; i++){
-                prices.push(parseFloat(collection[i].price));
-                dates.push(collection[i].date);
-            }
-//------------- chart ------------------------
+        }
+
+        var initializeChart = function() {
             $scope.chartConfig = {
+                options: {
+                    chart: {
+                        type: 'line'
+                    },
+                    tooltip: {
+                        style: {
+                            padding: 10,
+                            fontWeight: 'bold'
+                        }
+                    }
+                },
+                series: [{
+                    showInLegend: false,
+                    data: $scope.prices
+                }],
+                title: {
+                    text: 'Stock prices'
+                },
+                loading: false,
+                xAxis: {
+                    title: {
+                        text: 'Date'
+                    },
+                    categories: $scope.dates
+                },
 
-              options: {
-                  //This is the Main Highcharts chart config. Any Highchart options are valid here.
-                  //will be overriden by values specified below.
-                  chart: {
-                      type: 'line'
-                  },
-                  tooltip: {
-                      style: {
-                          padding: 10,
-                          fontWeight: 'bold'
-                      }
-                  }
-              },
-              //The below properties are watched separately for changes.
-
-              //Series object (optional) - a list of series using normal Highcharts series options.
-              series: [{
-                    showInLegend : false,
-                    data : prices
-              }],
-              //Title configuration (optional)
-              title: {
-                 text: 'Stock prices'
-              },
-              //Boolean to control showing loading status on chart (optional)
-              //Could be a string if you want to show specific loading text.
-              loading: false,
-              //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
-              //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
-              xAxis: {
-              title: {text: 'Date'},
-              categories : dates
-              },
-
-              yAxis: {
-                  title: {text: 'Price'},
-              },
-              //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
-              useHighStocks: false,
-              //size (optional) if left out the chart will default to size of the div or something sensible.
-              size: {
-               height: 400
-              },
-              //function (optional)
-              func: function (chart) {
-               //setup some logic for the chart
-              }
+                yAxis: {
+                    title: {
+                        text: 'Price'
+                    }
+                },
+                useHighStocks: false,
+                size: {
+                    height: 400
+                }
             };
-//------------------ end of chart -------------------------
+        }
 
+        tdpCompareService.getData().then(function(response) {
+                $scope.stockData = response.data;
+                $scope.prices = [];
+                $scope.dates = [];
+                for (i = 0; i < $scope.stockData.length; i++) {
+                    $scope.prices.push(parseFloat($scope.stockData[i].price));
+                    $scope.dates.push($scope.stockData[i].date);
+                }
+                initializeTable();
+                initializeChart();
         });
-    }, function(error) {
-        // Data income fails
-        $scope.message = error;
     });
 });
