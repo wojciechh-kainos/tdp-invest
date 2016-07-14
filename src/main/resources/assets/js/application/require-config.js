99d543a3556ba47fce2df9cbc8983a9d9fb381b0
Mateusz Czarnowski <m.czarnowski@kainos.com>
2016-07-14 15:31:05 +0200

    Delete obsolete dependencies

--- src/main/resources/assets/js/application/require-config.js
+++ src/main/resources/assets/js/application/require-config.js
@@ -7,7 +7,6 @@ require.config({
         'uiRouter': 'lib/angular-ui-router/release/angular-ui-router.min',
         'highcharts-ng': 'lib/highcharts-ng/dist/highcharts-ng.min',
         'highcharts': 'http://code.highcharts.com/highcharts.src',
-//        'angular-bootstrap': 'http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.12.1.min',
         'ui-bootstrap' : 'lib/angular-bootstrap/ui-bootstrap-tpls',
         'angularMocks': 'lib/angular-mocks/angular-mocks',
         'eonasdan-bootstrap-datetimepicker' : 'lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker'
@@ -19,7 +18,6 @@ require.config({
         },
         'uiRouter' : ['angular'],
         'highcharts-ng': ['angular', 'highcharts'],
-//        'angular-bootstrap': ['angular'],
         'ui-bootstrap' : ['angular']
     },
     deps: ['application/bootstrap']
