952172f6379e276d934a8f2861d02160c6eee1e1
Daniel Rauf <danielr@kainos.com>
2016-07-15 13:55:56 +0200

    Remove unnecessary dataLoading variable

--- src/main/resources/assets/js/application/controllers/tdpInvestLoginController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestLoginController.js
@@ -1,20 +1,18 @@
-define(['angular', 'application/tdpInvestModule', 'application/services/tdpAuthService'], function(angular, tdpInvestModule) {
-    tdpInvestModule.controller("tdpInvestLoginController", function($scope, $rootScope, $location, $stateParams, tdpAuthService) {
+define(['angular', 'application/tdpInvestModule', 'application/services/tdpAuthService'], function (angular, tdpInvestModule) {
+    tdpInvestModule.controller("tdpInvestLoginController", function ($scope, $rootScope, $location, $stateParams, tdpAuthService) {
 
         tdpAuthService.ClearCredentials();
 
         $scope.login = function () {
-            $scope.dataLoading = true;
-            tdpAuthService.Login($scope.username, $scope.password)
-                .then( function(response){
-                if(response.success) {
-                    tdpAuthService.SetCredentials($scope.username, $scope.password);
-                    $location.path('/tdp');
-                } else {
-                    $scope.error = response.message;
-                    $scope.dataLoading = false;
-                }
-            });
+            $scope.loginPromise = tdpAuthService.Login($scope.username, $scope.password)
+                .then(function (response) {
+                    if (response.success) {
+                        tdpAuthService.SetCredentials($scope.username, $scope.password);
+                        $location.path('/tdp');
+                    } else {
+                        $scope.error = response.message;
+                    }
+                });
         };
 
     });
--- src/main/resources/assets/js/application/controllers/tdpInvestRegisterController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestRegisterController.js
@@ -1,17 +1,14 @@
-define(['angular', 'application/tdpInvestModule', 'application/services/tdpAuthService'], function(angular, tdpInvestModule) {
-    tdpInvestModule.controller("tdpInvestRegisterController", function($location, $scope, tdpAuthService) {
+define(['angular', 'application/tdpInvestModule', 'application/services/tdpAuthService'], function (angular, tdpInvestModule) {
+    tdpInvestModule.controller("tdpInvestRegisterController", function ($location, $scope, tdpAuthService) {
 
-        $scope.register = function() {
-            $scope.dataLoading = true;
-            tdpAuthService.Register($scope.user.username, $scope.user.password)
-                .then(function(response) {
-                    if(response.success) {
+        $scope.register = function () {
+            $scope.registerPromise = tdpAuthService.Register($scope.user.username, $scope.user.password)
+                .then(function (response) {
+                    if (response.success) {
                         tdpAuthService.SetCredentials($scope.user.username, $scope.user.password);
                         $location.path('/tdp');
-                     }
-
+                    }
                     else {
-                        $scope.dataLoading = false;
                         $scope.error = response.message;
                     }
                 });
