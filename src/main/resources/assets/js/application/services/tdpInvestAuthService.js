326a81406c721bbfc76dfccf9d0a6fcfce288b34
mntss <mateusz.piotrowski94@gmail.com>
2016-07-18 11:48:33 +0200

    Fix safari problem Change links in navbar

--- src/main/resources/assets/js/application/services/tdpInvestAuthService.js
+++ src/main/resources/assets/js/application/services/tdpInvestAuthService.js
@@ -1,12 +1,12 @@
 define(['angular','application/tdpInvestModule', 'application/services/tdpInvestBase64Service', 'ngCookies'], function(angular, tdpInvestModule) {
     tdpInvestModule.service('tdpInvestAuthService', ['$cookieStore', '$http', '$rootScope', 'tdpInvestBase64Service', function($cookieStore, $http, $rootScope, tdpInvestBase64Service) {
-        var service = {};
 
-        service.login = function(username, password) {
+
+        this.login = function(username, password) {
             return $http.post('/api/login', { mail: username, password: password }).then(handleSuccess, handleError('Bad credentials'));
         };
 
-        service.register = function(username, password) {
+        this.register = function(username, password) {
             return $http.post('/api/register',  { mail: username, password: password }).then(handleSuccess, function (response) {
                 if(response.status == 409){
                     return { success: false, message: "Email address already in use." };
@@ -15,7 +15,7 @@ define(['angular','application/tdpInvestModule', 'application/services/tdpInvest
             });
         };
 
-        service.setCredentials = function(email, password) {
+        this.setCredentials = function(email, password) {
             var authdata = tdpInvestBase64Service.encode(email + ':' + password);
 
             $rootScope.globals = {
@@ -29,13 +29,13 @@ define(['angular','application/tdpInvestModule', 'application/services/tdpInvest
             $cookieStore.put('globals', $rootScope.globals);
         };
 
-        service.clearCredentials = function() {
+        this.clearCredentials = function() {
             $rootScope.globals = {};
             $cookieStore.remove('globals');
-            $http.defaults.headers.common.Authorization = 'Basic ';
+            $http.defaults.headers.common['Authorization'] = '';
         };
 
-        service.watchAuthorizationStatus = function(loginCallback, logoutCallback) {
+        this.watchAuthorizationStatus = function(loginCallback, logoutCallback) {
             $rootScope.$watch('globals.currentUser', function(newVal) {
                 if (newVal) { // user logged in
                     loginCallback();
@@ -58,6 +58,6 @@ define(['angular','application/tdpInvestModule', 'application/services/tdpInvest
             };
         }
 
-        return service;
+
     }]);
 });
