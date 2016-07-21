25f2b11b24d18d2b51c22c106692a5f4a267a55c
Dawid Kręczkowski <d.kreczkowski@kainos.com>
2016-07-21 08:58:58 +0200

    change TimeSeries schema. clean CSVParser class

--- src/main/java/helpers/CSVParser.java
+++ src/main/java/helpers/CSVParser.java
@@ -7,12 +7,6 @@ import java.io.*;
 
 
 public final class CSVParser {
-
-    public static void main(String[] args) throws FileNotFoundException, IOException, JSONException {
-        InputStream in = new FileInputStream(new File("/Users/dawidk/Desktop/result.csv"));
-        System.out.println(CSVtoJSON(in));
-    }
-
     public static String CSVtoJSON(InputStream input) throws JSONException, IOException {
         System.out.println("CSVtoJSON running!");
         long x;
@@ -31,5 +25,4 @@ public final class CSVParser {
         }
         return output.toString();
     }
-
 }
--- src/main/java/helpers/TimeSeries.java
+++ src/main/java/helpers/TimeSeries.java
@@ -1,39 +1,33 @@
 package helpers;
 
-import model.Fund;
 import org.joda.time.DateTime;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class TimeSeries {
-    List<Fund> timeSeries;
+public final class TimeSeries {
+    public static String createTimeSeries(DateTime startDate, DateTime endDate, double amount, double annualRate) throws JSONException {
+        DateTime date=startDate;
+        double value;
+        JSONArray point;
+        JSONArray output = new JSONArray();
 
-    public TimeSeries(DateTime startDate, DateTime endDate, double amount, double annualRate){
-        this.timeSeries = createTimeSeries(startDate, endDate, amount, annualRate);
+        while(!date.isAfter(endDate)){
+            point = new JSONArray();
+            value = futureValue(datesDiffInDays(startDate, date), amount, annualRate);
+            point.put(date);
+            point.put(value);
+            output.put(point);
+            date = date.plusDays(1);
+        }
+        return output.toString();
     }
 
-    public List<Fund> getTimeSeries(){
-        return this.timeSeries;
-    }
-
-    private List<Fund> createTimeSeries(DateTime startDate, DateTime endDate, double amount, double annualRate){
-        return Stream
-                .iterate(startDate, date -> date.plusDays(1))
-                .limit(datesDiffInDays(startDate, endDate) + 2)
-                .skip(1)
-                .map(row -> new Fund(row, futureValue(datesDiffInDays(startDate, row), amount, annualRate)))
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    private double futureValue(long days, double amount, double annualRate){
+    private static double futureValue(long days, double amount, double annualRate){
         return amount * Math.pow(1 + annualRate / 365.25, days + 1);
     }
 
-    private long datesDiffInDays(DateTime startDate, DateTime endDate){
+    private static long datesDiffInDays(DateTime startDate, DateTime endDate){
         return TimeUnit.DAYS.convert(endDate.getMillis() - startDate.getMillis(), TimeUnit.MILLISECONDS);
     }
 }
