4ba117264a945185a8eb3d6f7becdbc8bd99d90f
Mateusz Czarnowski <m.czarnowski@kainos.com>
2016-07-21 16:44:02 +0200

    Remove obsolete code

--- src/test/java/framework/TestModule.java
+++ src/test/java/framework/TestModule.java
@@ -1,12 +1,9 @@
 package framework;
 
 import com.google.inject.AbstractModule;
-import services.DummyJob;
-import services.SecondImplementation;
 
 public class TestModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(DummyJob.class).to(SecondImplementation.class);
     }
 }
