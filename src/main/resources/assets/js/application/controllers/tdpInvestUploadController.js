971aaa0cb39e9403362729763ae8a36684185088
Jakub Raniszewski <j.raniszewski@kainos.com>
2016-07-25 10:24:33 +0200

    add error message when upload failed

--- src/main/resources/assets/js/application/controllers/tdpInvestUploadController.js
+++ src/main/resources/assets/js/application/controllers/tdpInvestUploadController.js
@@ -1,6 +1,7 @@
 define(['angular', 'application/tdpInvestModule', 'angular-file-upload', 'ngCookies', 'application/services/tdpInvestBase64Service'], function (angular, tdpInvestModule) {
     tdpInvestModule.controller("tdpInvestUploadController", function ($scope, $state, FileUploader, $http, $cookieStore, tdpInvestBase64Service) {
         $scope.showGif = false;
+        $scope.error = false;
         $scope.uploader = new FileUploader();
         $scope.uploader.url="/api/unit/upload";
         $scope.uploader.headers = {'Authorization' : 'Basic ' + $cookieStore.get("currentUser").authdata};
@@ -11,6 +12,12 @@ define(['angular', 'application/tdpInvestModule', 'angular-file-upload', 'ngCook
 
         $scope.uploader.onCompleteAll = function(){
             $state.go('main');
+            $scope.error = false;
+        }
+
+        $scope.uploader.onErrorItem = function(){
+            $scope.error = true;
+            $scope.gif = false;
         }
 
     });
